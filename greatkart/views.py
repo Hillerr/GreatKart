21ee057a670ddebe2d6cfa35866ff4a4f45713a0
Hillerr <rafaelhiller23@gmail.com>
2021-06-23 22:42:39 -0300

    adding application files

---
+++ greatkart/views.py
@@ -0,0 +1,13 @@
+from category.models import Category
+from django.shortcuts import get_object_or_404, render
+from store.models import Product
+
+def home(request):
+
+    products = Product.objects.all().filter(is_available=True)
+
+    context = {
+        'products': products
+    }
+
+    return render(request, 'index.html', context=context)
