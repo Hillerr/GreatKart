c1423cbbca30566686cd93cedbdf75d4deecd435
Hillerr <rafaelhiller23@gmail.com>
2021-07-07 19:53:23 -0300

    Handled cart checkout page for the logged users

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,12 @@
+from carts.models import Cart, CartItem
 import accounts
 from accounts.models import Account
 from django.shortcuts import redirect, render
 from .forms import RegistrationForm
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
+from carts.views import _cart_id
+import requests
 
 # Verification email imports
 from django.contrib.sites.shortcuts import get_current_site
@@ -51,12 +54,62 @@ def login(request):
     if request.method == 'POST':
         email = request.POST['email']
         password = request.POST['password']
-        print(email, password)
         user = auth.authenticate(email=email, password=password)
-        print(user)
 
         if user is not None:
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+
+                    product_variation = []
+                    for item in cart_item:
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+
+                    # Get the cart items from the user to access his product variations
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list = []
+                    id = []
+
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = CartItem.objects.get(id=item_id)
+                            item.quantity += 1
+                            item.user = user
+                            item.save()
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+
+                            for item in cart_item:
+                                item.user = user 
+                                item.save()
+
+            except:
+                pass
+
             auth.login(request, user)
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                # next = /carts/checkout/
+                # create a dict with next as key
+                params = dict(x.split('=') for x in query.split('&'))
+
+                if 'next' in params.keys():
+                    next_page = params['next']
+                    return redirect(next_page)
+            except:
+                pass
             return redirect('dashboard')
 
         else:
--- static/js/script.js
+++ static/js/script.js
@@ -61,3 +61,28 @@ $(document).ready(function() {
 setTimeout(function(){
     $('message').fadeOut('slow')
 }, 4000)
+
+
+function Trim(strTexto)
+        {
+            // Substitúi os espaços vazios no inicio e no fim da string por vazio.
+            return strTexto.replace(/^s+|s+$/g, '');
+        }
+ 
+    // Função para validação de CEP.
+    function IsCEP(strCEP, blnVazio)
+        {
+            // Caso o CEP não esteja nesse formato ele é inválido!
+            var objER = /^[0-9]{2}.[0-9]{3}-[0-9]{3}$/;
+ 
+            strCEP = Trim(strCEP)
+            if(strCEP.length > 0)
+                {
+                    if(objER.test(strCEP))
+                        return true;
+                    else
+                        return false;
+                }
+            else
+                return blnVazio;
+        }
